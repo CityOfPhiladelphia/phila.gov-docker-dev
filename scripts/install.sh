0d14980370df9de31c2295b74dfdf1bf30ed00f5
Alejandro Lopez <alejandro.lopez@phila.gov>
2019-07-08 14:12:35 -0400

    Removing unneded commented code

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,27 +23,12 @@ while [[ -n "$isfree" ]]; do
   isfree=$(lsof -nP -i4TCP:$port | grep LISTEN)
 done
 
-# DBBASE=3307
-
-# dbport=$DBBASE
-# isfree=$(lsof -nP -i4TCP:$dbport | grep LISTEN)
-
-# while [[ -n "$isfree" ]]; do
-#   dbport=$[dbport+1]
-#   isfree=$(lsof -nP -i4TCP:$dbport | grep LISTEN)
-# done
-
-# appname=$(sudo base64 /dev/urandom | tr -d '/+' | dd bs=5 count=1 2>/dev/null)
-
 echo "Web Port: $port"
 echo "Name: philagov-$port"
-
-# echo "Db Port: $dbport"
 
 export INSTALATION_PORT=$port
 export INSTALATION_NAME="philagov-$port"
 export PHILAGOV_IMAGE="$image_name:$image_version"
-# export DB_INSTALLATION_PORT=$dbport
 
 COMMAND="docker-compose run --service-ports --name $INSTALATION_NAME philagov"
 echo "$COMMAND"
