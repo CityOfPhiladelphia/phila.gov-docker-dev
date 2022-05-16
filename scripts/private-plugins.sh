aabb6f98c9cbcacfa885ed15e771c606ebb68d69
Karissa Demi <karissa.demi@phila.gov>
2022-05-16 16:15:59 -0400

    Update plugin list

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -7,16 +7,18 @@ fi
 
 printf $'\e[33mInstalling private plugins\e[0m\n'
 
-plugins="mb-admin-columns-1.5.0.zip
+plugins="mb-admin-columns-1.6.0.zip
+mb-rest-api-1.4.0.zip
 mb-revision-1.3.3.zip
-mb-settings-page-2.1.3.zip
-mb-term-meta-1.2.9.zip
-meta-box-columns-1.2.7.zip
-meta-box-conditional-logic-1.6.13.zip
-meta-box-group-1.3.11.zip
+mb-settings-page-2.1.4.zip
+mb-term-meta-1.2.10.zip
+meta-box-columns-1.2.14.zip
+meta-box-conditional-logic-1.6.14.zip
+meta-box-group-1.3.12.zip
 meta-box-include-exclude-1.0.11.zip
-meta-box-tabs-1.1.8.zip
-meta-box-tooltip-1.1.3.zip
+meta-box-tabs-1.1.9.zip
+wp-nested-pages-phila-3.1.15.zip
+meta-box-tooltip-1.1.4.zip
 wpfront-user-role-editor-personal-pro-2.14.5.zip"
 
 pushd /var/www/html/wp-content/plugins
